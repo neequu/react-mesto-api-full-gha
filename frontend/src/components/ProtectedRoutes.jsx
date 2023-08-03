d9f29a6619fc78767131a7482060d91779487d3b
nick <69749247+niccc0@users.noreply.github.com>
2023-08-03 23:43:15 +0300

    Delete frontend directory

--- frontend/src/components/ProtectedRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoutes = ({ loggedIn }) => {
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
-};
-
-export default ProtectedRoutes;
--- frontend/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-// settings for validation
-const settings = {
-    inputSelector: '.form__input',
-    submitButtonSelector: '.form__submit-button',
-    inputErrorClass: 'form__input_type_error',
-    errorClass: 'form__input-error_active'
-}
-
-export {settings}
