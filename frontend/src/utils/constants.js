02c96967f40f1564a8edafa49d333c84020358bd
nikita stepanov <nist1701@gmail.com>
2023-08-04 15:04:19 +0300

    fixes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -6,4 +6,4 @@ export const settings = {
     errorClass: 'form__input-error_active'
 }
 
-export const baseUrl = 'http://api.neequu.nomoreparties.co/'
+export const baseUrl = 'http://api.neequu.nomoreparties.co'
